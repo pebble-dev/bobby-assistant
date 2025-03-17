d05377436347a4889b4f30fd2b545e45c13d9baf
Katharine Berry <ktbry@google.com>
2025-03-16 20:52:26 -0700

    Fix inadvertently changed URLs.
    Signed-off-by: Katharine Berry <ktbry@google.com>

--- app/src/pkjs/urls.js
+++ app/src/pkjs/urls.js
@@ -15,5 +15,5 @@
  */
 
 // put your server here.
-exports.QUERY_URL = 'ws://192.168.0.193:8080/query';
-exports.QUOTA_URL = 'http://192.168.0.193:8080/quota';
+exports.QUERY_URL = 'wss://bobby-api.rebble.io/query';
+exports.QUOTA_URL = 'https://bobby-api.rebble.io/quota';
