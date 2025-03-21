2585332e95702a2de2c33ac2200843ec5655396a
Katharine Berry <ktbry@google.com>
2025-03-20 23:14:25 -0700

    Add formatting to the legal page.
    Signed-off-by: Katharine Berry <ktbry@google.com>

--- app/src/c/menus/legal_window.c
+++ app/src/c/menus/legal_window.c
@@ -17,10 +17,11 @@
 #include "legal_window.h"
 #include <pebble.h>
 #include "../util/style.h"
+#include "../util/formatted_text_layer.h"
 
 typedef struct {
  char *legal_text;
- TextLayer *text_layer;
+ FormattedTextLayer *text_layer;
  ScrollLayer *scroll_layer;
  StatusBarLayer *status_bar;
 } CreditsWindowData;
@@ -49,25 +50,24 @@ static void prv_window_load(Window* window) {
  data->legal_text[res_size] = '\0';
  Layer *root_layer = window_get_root_layer(window);
  GRect window_bounds = layer_get_bounds(root_layer);
- GSize text_size = graphics_text_layout_get_content_size(data->legal_text, fonts_get_system_font(FONT_KEY_GOTHIC_24), GRect(5, 0, window_bounds.size.w - 10, 10000), GTextOverflowModeWordWrap, GTextAlignmentLeft);
  data->status_bar = status_bar_layer_create();
  bobby_status_bar_config(data->status_bar);
  layer_add_child(root_layer, status_bar_layer_get_layer(data->status_bar));
  data->scroll_layer = scroll_layer_create(GRect(0, STATUS_BAR_LAYER_HEIGHT, window_bounds.size.w, window_bounds.size.h - STATUS_BAR_LAYER_HEIGHT));
  scroll_layer_set_shadow_hidden(data->scroll_layer, true);
  scroll_layer_set_click_config_onto_window(data->scroll_layer, window);
- scroll_layer_set_content_size(data->scroll_layer, GSize(window_bounds.size.w, text_size.h + 10));
  layer_add_child(root_layer, scroll_layer_get_layer(data->scroll_layer));
- data->text_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 10, text_size.h + 5));
- text_layer_set_font(data->text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
- text_layer_set_text(data->text_layer, data->legal_text);
- scroll_layer_add_child(data->scroll_layer, text_layer_get_layer(data->text_layer));
+ data->text_layer = formatted_text_layer_create(GRect(5, 0, window_bounds.size.w - 10, 10000));
+ formatted_text_layer_set_text(data->text_layer, data->legal_text);
+ GSize text_size = formatted_text_layer_get_content_size(data->text_layer);
+ scroll_layer_set_content_size(data->scroll_layer, GSize(window_bounds.size.w, text_size.h + 10));
+ scroll_layer_add_child(data->scroll_layer, formatted_text_layer_get_layer(data->text_layer));
 }
 
 static void prv_window_unload(Window* window) {
  CreditsWindowData *data = window_get_user_data(window);
  free(data->legal_text);
- text_layer_destroy(data->text_layer);
+ formatted_text_layer_destroy(data->text_layer);
  scroll_layer_destroy(data->scroll_layer);
  status_bar_layer_destroy(data->status_bar);
  free(data);
