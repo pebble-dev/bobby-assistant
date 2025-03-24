a5ac3291e1dcc878c1935d0ad24dd2031ed825b4
Katharine Berry <ktbry@google.com>
2025-03-23 23:59:25 -0700

    Comment about previous turns no longer relevant.
    Signed-off-by: Katharine Berry <ktbry@google.com>

--- service/assistant/system_prompt.go
+++ service/assistant/system_prompt.go
@@ -54,6 +54,8 @@ func generateLanguageSentence(ctx context.Context) string {
 	var language = util.GetLanguageName(query.PreferredLanguageFromContext(ctx))
 	if language != "" {
 		sentence += "Respond in " + language + ". "
+	} else {
+		sentence += "Respond in the language the user is using, unless they specify otherwise."
 	}
 	return sentence
 }
@@ -76,10 +78,19 @@ func generateWidgetSentence(ctx context.Context) string {
 	}
 	sentence := "You can embed some widgets in your responses by using a special syntax. The following widgets are available:\n"
 	if query.SupportsWidget(ctx, "weather") {
-		sentence += "<!WEATHER-CURRENT location=[here|place name] units=[metric|imperial|uk hybrid]!>: embeds a weather widget showing the weather right now in the given location\n" +
-			"<!WEATHER-SINGLE-DAY location=[here|place name] units=[metric|imperial|uk hybrid] day=[the name of a weekday, like Tuesday]!>: embeds a weather widget summarising the weather in the given location for a single day within the coming week.\n" +
-			"<!WEATHER-MULTI-DAY location=[here|place name] units=[metric|imperial|uk hybrid]!>: embeds a weather widget summarising the weather in the given location for the next three days\n" +
-			"Before including a weather widget, you *must* still look up the weather, and include a textual response after the widget. Always call get_weather first, then put the widget before any other text. If showing the weather for the user's current location, always use 'here' instead of a place name. If asked for only one day of weather, don't respond with multiple days.\n\n"
+		has_location := query.LocationFromContext(ctx) != nil
+		location_value := "place name"
+		if has_location {
+			location_value = "here|place name"
+		}
+		sentence += "<!WEATHER-CURRENT location=[" + location_value + "] units=[metric|imperial|uk hybrid]!>: embeds a weather widget showing the weather right now in the given location\n" +
+			"<!WEATHER-SINGLE-DAY location=[" + location_value + "] units=[metric|imperial|uk hybrid] day=[the name of a weekday, like Tuesday]!>: embeds a weather widget summarising the weather in the given location for a single day within the coming week.\n" +
+			"<!WEATHER-MULTI-DAY location=[" + location_value + "] units=[metric|imperial|uk hybrid]!>: embeds a weather widget summarising the weather in the given location for the next three days\n" +
+			"Before including a weather widget, you *must* still look up the weather, and include a textual response after the widget. Always call get_weather first, then put the widget before any other text. "
+		if has_location {
+			sentence += "If showing the weather for the user's current location, always use 'here' instead of a place name. "
+		}
+		sentence += "If asked for only one day of weather, don't respond with multiple days.\n\n"
 	}
 	if query.SupportsWidget(ctx, "timer") {
 		sentence += "<!TIMER targetTime=[time in ISO 8601 format] name=[name of the timer]!>: embeds a timer widget counting down to the given time. If the timer doesn't have a name, the `name` field can be omitted\n" +
@@ -117,7 +128,6 @@ func (ps *PromptSession) generateSystemPrompt(ctx context.Context) string {
 		"If the user asks to set an alarm, assume they always want to set it for a time in the future. " +
 		"As a creative, intelligent, helpful, friendly assistant, you should always try to answer the user's question. You can and should provide creative suggestions and factual responses as appropriate. Always try your best to answer the user's question. " +
 		"**Never** claim to have taken an action (e.g. set a timer, alarm, or reminder) unless you have actually used a tool to do so. " +
-		"Even if in previous turns you have apparently taken an action (like setting an alarm) without using a tool, you must still use tools if asked to do so again. " +
 		"Alarms and reminders are not interchangable - *never* use alarms when a user asks for reminders, and never user reminders when the user asks for an alarm or timer. If a user asks to set a timer, always set a timer (using 'set_timer'), not a reminder. If the user asks about a specific timer, respond only about that one. " +
 		"If asked to perform language translation (e.g. 'what is X in french?'), *don't* look anything up - just respond immediately. You know how to do translations between any language pair. " +
 		"Your responses will be displayed on a very small screen, so be brief. Do not use markdown in your responses.\n" +
