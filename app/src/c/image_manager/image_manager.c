9e1ca71807ba4f66e5e92285be0ea5d0ede3d54f
Katharine Berry <ktbry@google.com>
2025-04-14 21:04:51 -0700

    Revert image manager to dynamic version.
    Signed-off-by: Katharine Berry <ktbry@google.com>

--- app/src/c/image_manager/image_manager.c
+++ app/src/c/image_manager/image_manager.c
@@ -36,19 +36,24 @@ typedef struct {
 
 static void prv_inbox_received(DictionaryIterator *iterator, void *context);
 static ManagedImage *prv_find_image(int image_id);
-static void prv_destroy_image();
+static int16_t prv_find_image_index(int image_id);
+static bool prv_image_id_compare(void *image_id, void *object);
+static void prv_destroy_image(ManagedImage *image);
+static bool prv_foreach_destroy(void *object, void *context);
 static void prv_handle_new_image(int image_id, size_t size, DictionaryIterator *iterator);
 static void prv_handle_image_chunk(int image_id, size_t offset, DictionaryIterator *iterator);
 static void prv_handle_image_complete(int image_id);
+static bool prv_handle_memory_pressure(void *context);
 
-
-static ManagedImage s_managed_image = {0};
-static uint8_t s_image_storage[PBL_IF_COLOR_ELSE(3800, 1900)] = {0};
 static EventHandle *s_appmessage_handle;
+static LinkedRoot *s_image_list;
+static ManagedImage *s_cached_image_ref = NULL;
 
 void image_manager_init() {
+  s_image_list = linked_list_create_root();
   events_app_message_request_inbox_size(1024);
   s_appmessage_handle = events_app_message_register_inbox_received(prv_inbox_received, NULL);
+  memory_pressure_register_callback(prv_handle_memory_pressure, 0, NULL);
 }
 
 void image_manager_deinit() {
@@ -89,32 +94,63 @@ GSize image_manager_get_size(int image_id) {
 }
 
 void image_manager_destroy_image(int image_id) {
-  if (s_managed_image.image_id != image_id) {
+  int16_t image_idx = prv_find_image_index(image_id);
+  if (image_idx < 0) {
     return;
   }
-  prv_destroy_image();
+  ManagedImage *image = linked_list_get(s_image_list, image_idx);
+  prv_destroy_image(image);
+  linked_list_remove(s_image_list, image_idx);
 }
 
 void image_manager_destroy_all_images() {
-  prv_destroy_image();
+  linked_list_foreach(s_image_list, prv_foreach_destroy, NULL);
+  linked_list_clear(s_image_list);
 }
 
 static ManagedImage *prv_find_image(int image_id) {
-  if (s_managed_image.image_id != image_id) {
-    return NULL;
+  if (s_cached_image_ref != NULL && s_cached_image_ref->image_id == image_id) {
+    return s_cached_image_ref;
   }
-  return &s_managed_image;
+  int16_t idx = prv_find_image_index(image_id);
+  if (idx >= 0) {
+    return linked_list_get(s_image_list, idx);
+  }
+  return NULL;
 }
 
-static void prv_destroy_image() {
-  s_managed_image.status = ImageStatusDestroyed;
-  if (s_managed_image.callback) {
-    s_managed_image.callback(s_managed_image.image_id, ImageStatusDestroyed, s_managed_image.context);
+static int16_t prv_find_image_index(int image_id) {
+  return linked_list_find_compare(s_image_list, &image_id, prv_image_id_compare);
+}
+
+static bool prv_image_id_compare(void *image_id, void *object) {
+  ManagedImage *image = object;
+  return image->image_id == *(int *)image_id;
+}
+
+static void prv_destroy_image(ManagedImage *image) {
+  image->status = ImageStatusDestroyed;
+  if (image->callback) {
+    image->callback(image->image_id, ImageStatusDestroyed, image->context);
   }
-  if (s_managed_image.bitmap) {
-    gbitmap_destroy(s_managed_image.bitmap);
-    s_managed_image.bitmap = NULL;
+  if (image->bitmap) {
+    gbitmap_destroy(image->bitmap);
+    image->bitmap = NULL;
   }
+  if (image->data) {
+    free(image->data);
+    image->data = NULL;
+  }
+  if (s_cached_image_ref == image) {
+    s_cached_image_ref = NULL;
+  }
+  free(image);
+}
+
+static bool prv_foreach_destroy(void *object, void *context) {
+  ManagedImage *image = object;
+  prv_destroy_image(image);
+  return true;
 }
 
 static void prv_inbox_received(DictionaryIterator *iterator, void *context) {
@@ -147,16 +183,22 @@ static void prv_handle_new_image(int image_id, size_t size, DictionaryIterator *
   tuple = dict_find(iterator, MESSAGE_KEY_IMAGE_HEIGHT);
   int16_t height = tuple->value->int32;
   BOBBY_LOG(APP_LOG_LEVEL_DEBUG, "New image: %d, size: %d, width: %d, height: %d", image_id, size, width, height);
-  if (s_managed_image.image_id != 0) {
-    prv_destroy_image();
+  ManagedImage *image = bmalloc(sizeof(ManagedImage));
+  if (!image) {
+    BOBBY_LOG(APP_LOG_LEVEL_WARNING, "Failed to allocate memory for image metadata");
+    return;
   }
-  s_managed_image.image_id = image_id;
-  s_managed_image.status = ImageStatusCreated;
-  s_managed_image.callback = NULL;
-  s_managed_image.data = s_image_storage;
-  s_managed_image.size = size;
-  s_managed_image.bitmap = NULL;
-  s_managed_image.image_size = GSize(width, height);
+  image->data = bmalloc(size);
+  if (!image->data) {
+    BOBBY_LOG(APP_LOG_LEVEL_WARNING, "Failed to allocate memory for image data");
+  }
+  image->image_id = image_id;
+  image->status = ImageStatusDestroyed;
+  image->callback = NULL;
+  image->size = size;
+  image->bitmap = NULL;
+  image->image_size = GSize(width, height);
+  linked_list_append(s_image_list, image);
 }
 
 static void prv_handle_image_chunk(int image_id, size_t offset, DictionaryIterator *iterator) {
@@ -164,6 +206,10 @@ static void prv_handle_image_chunk(int image_id, size_t offset, DictionaryIterat
   ManagedImage *image = prv_find_image(image_id);
   if (!image) {
     BOBBY_LOG(APP_LOG_LEVEL_INFO, "Got data for unknown image id %d", image_id);
+    return;
+  }
+  if (!image->data) {
+    BOBBY_LOG(APP_LOG_LEVEL_INFO, "Got data for image we couldn't allocate; discarding.");
     return;
   }
   Tuple *tuple = dict_find(iterator, MESSAGE_KEY_IMAGE_CHUNK_DATA);
@@ -186,6 +232,12 @@ static void prv_handle_image_complete(int image_id) {
     BOBBY_LOG(APP_LOG_LEVEL_WARNING, "Got complete for unknown image id %d", image_id);
     return;
   }
+  if (!image->data) {
+    BOBBY_LOG(APP_LOG_LEVEL_INFO, "Got complete for image we couldn't allocate; destroying.");
+    prv_destroy_image(image);
+    linked_list_remove(s_image_list, prv_find_image_index(image_id));
+    return;
+  }
   if (!image->bitmap) {
     image->bitmap = gbitmap_create_with_data(image->data);
     if (!image->bitmap) {
@@ -205,4 +257,16 @@ static void prv_handle_image_complete(int image_id) {
   if (image->callback) {
     image->callback(image->image_id, ImageStatusCompleted, image->context);
   }
+}
+
+
+static bool prv_handle_memory_pressure(void *context) {
+  if (linked_list_count(s_image_list) == 0) {
+    return false;
+  }
+  BOBBY_LOG(APP_LOG_LEVEL_WARNING, "Memory pressure! Destroying the oldest image.");
+  ManagedImage *image = linked_list_get(s_image_list, 0);
+  prv_destroy_image(image);
+  linked_list_remove(s_image_list, 0);
+  return true;
 }
