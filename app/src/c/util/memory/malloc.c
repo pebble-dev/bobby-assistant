33a6f3f9aa042c11d39b07ba1d0743119ed0b017
Katharine Berry <ktbry@google.com>
2025-04-19 11:29:21 -0700

    Log lr from bmalloc to assist in memory tracking.
    Signed-off-by: Katharine Berry <ktbry@google.com>

--- app/src/c/util/memory/malloc.c
+++ app/src/c/util/memory/malloc.c
@@ -21,6 +21,8 @@
 #include <pebble.h>
 
 void *bmalloc(size_t size) {
+  register uintptr_t lr __asm("lr");
+  const uintptr_t saved_lr = lr;
   int heap_size = heap_bytes_free();
   BOBBY_LOG(APP_LOG_LEVEL_DEBUG, "malloc request: %d; free: %d", size, heap_size);
   while (true) {
@@ -28,6 +30,7 @@ void *bmalloc(size_t size) {
     if (heap_bytes_free() > 750) {
       void *ptr = malloc(size);
       if (ptr) {
+        BOBBY_LOG(APP_LOG_LEVEL_DEBUG_VERBOSE, "malloc returned %p for caller %p", ptr, saved_lr);
         return ptr;
       }
       BOBBY_LOG(APP_LOG_LEVEL_WARNING, "Out of memory! Need to allocate %d bytes; %d bytes free.", size, heap_size);
@@ -36,7 +39,11 @@ void *bmalloc(size_t size) {
     }
     if (!memory_pressure_try_free()) {
       BOBBY_LOG(APP_LOG_LEVEL_ERROR, "Failed to allocate memory: couldn't free enough heap.");
-      return malloc(size);
+      void *tried = malloc(size);
+      if (tried) {
+        BOBBY_LOG(APP_LOG_LEVEL_DEBUG, "malloc returned %p for caller %p", tried, saved_lr);
+      }
+      return tried;
     }
     int new_heap_size = heap_bytes_free();
     BOBBY_LOG(APP_LOG_LEVEL_INFO, "Freed %d bytes, heap size is now %d. Retrying allocation of %d bytes.", heap_size - new_heap_size, new_heap_size, size);
